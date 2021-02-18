c00a120fbd8005d31cdd607298ed5c8c201c8db1
lilokhang <lilokhang007@gmail.com>
2021-02-19 02:45:59 +0800

    Basic structure

---
+++ build.py
@@ -0,0 +1,17 @@
+from jinja2.exceptions import TemplateSyntaxError
+from staticjinja import Site
+from distutils.dir_util import copy_tree
+
+if __name__ == "__main__":
+	copy_tree('static', 'docs')
+	site = Site.make_site(
+		searchpath = 'src',
+		outpath = 'docs')
+	try:
+		site.render()
+	except TemplateSyntaxError as error:
+		print('Template syntax error:', error.filename, 'line', error.lineno)
+		print(error.message)
+
+	#os.system('staticrypt docs/tutorial1.html tko123;')
+	#move('docs/tutorial1_encrypted.html','docs/tutorial1.html')
