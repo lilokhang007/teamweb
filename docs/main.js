8a3e06c9a42b4a06b9b3a5d61e0d4b45edc14b5e
lilokhang <lilokhang007@gmail.com>
2021-02-23 01:35:17 +0800

    Scroll Navbar JS and Research DOM

--- docs/main.js
+++ docs/main.js
@@ -1,16 +1,9 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  var child = document.getElementById('nav').getElementsByTagName('*');
-
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    for (var i=0, len=child.length|0; i<len; i=i+1|0) {
-        child[i].style.height = "0px";
-    }
-  } else {
-    for (var i=0, len=child.length|0; i<len; i=i+1|0) {
-        child[i].style.height = "80px";
-    }
-  }
+/* Function to hide navbar when scroll is larger than 80 px*/
+window.onscroll = function() {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("nav").style.top = "-80px";
+      } else {
+        document.getElementById("nav").style.top = "0";
+      }
+      prevScrollpos = currentScrollPos;
 }
